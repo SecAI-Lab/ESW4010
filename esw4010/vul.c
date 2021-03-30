1b4065334ae2882e843d4eb95cd97ed1fe75fabb
Kevin Koo <kevinkoo001@gmail.com>
2021-03-30 10:47:43 +0900

    vul.c

---
+++ esw4010/vul.c
@@ -0,0 +1,20 @@
+// gcc -fno-stack-protector -z execstack -no-pie -o vul vul.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void vul(char* name) {
+        char buf[64] = {};
+        strcpy(buf, name);
+}
+
+int main(int argc, char* argv[]) {
+        if (argc < 2) {
+                printf ("Usage: %s [Your name]\n", argv[0]);
+                exit(-1);
+        }
+
+        vul(argv[1]);
+        return 0;
+}
+
